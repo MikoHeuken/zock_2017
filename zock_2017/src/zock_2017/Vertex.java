3370c808dcaae3dd8ca06452c55d26198383a2d7
Heuken.Miko <Heuken.Miko@SK-D8CB8A509AF5.2631.skool.net>
2017-10-06 09:12:20 +0200

    First commit.

--- zock_2017/src/zock_2017/Vertex.java
+++ zock_2017/src/zock_2017/Vertex.java
@@ -1,10 +1,23 @@
 package zock_2017;
 
 public class Vertex {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	
+	public	double x;
+	public	double y;
+	
+	
+	public Vertex(double x, double y){
+		
+		this.x = x;
+		this.y = y;
 	}
 
+	
+	public static void main(String[] args) {
+
+		Vertex v1 = new Vertex(2,2);
+		System.out.println(v1);
+		
+	}
 }
